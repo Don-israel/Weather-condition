58d24edc020632807ae05da0d2e525f6ab788fb6
Don_israel <israelojakorotu@gmail.com>
2024-11-04 19:29:04 +0100

    weather condition

---
+++ main.py
@@ -0,0 +1,11 @@
+def weather_condition():
+    print("Weather is pleasant in",spring)
+    print("Weather is same in",autumn)
+    print("weather is cold in",harmattan)
+    print("weather is Hot in",summer)
+
+spring="autumn"
+autumn="harmattan"
+harmattan="summer"
+summer="spring"
+weather_condition()    
